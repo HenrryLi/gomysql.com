a6480af360b7573f19bf081779c93fbe6c93c26d
Shengheng Li <shengheng.li@spp.com>
2020-06-08 10:47:04 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"goForMySQL/db"
+)
+
+func main() {
+	var con *sql.DB
+	fmt.Println("phpflow.com – Go MySQL Tutorial")
+	con := db.CreateCon()
+
+	// Execute the query
+	rows, err := con.Query("SELECT * FROM t1")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	fmt.Println("rows: ", rows)
+
+}
